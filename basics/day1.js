f22f5c4ffdbcaf89bfa48a851e0bc3be686e451d
Anjali_Ramesh <140633896+AnjaliiRamesh@users.noreply.github.com>
2024-07-29 03:28:16 +0530

    Add files via upload

---
+++ basics/day1.js
@@ -0,0 +1,28 @@
+//Activity-1
+//task 1
+var age = 19
+console.log("My age",age)
+age = 22
+console.log("changed value is",age)
+
+//task 2
+let myEmail = "anjali@gmail.com"
+console.log("My email is",myEmail)
+myEmail = "hdfc@gmail.com"
+console.log("Changed value is",myEmail)
+
+
+//Activity-2
+//task 3
+const value = false
+console.log("value is",value)
+// value = true
+console.log("changed value is",value)//this is not working as value in const can't be changed
+
+
+
+
+
+
+
+
